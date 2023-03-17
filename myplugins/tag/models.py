9d736f77aa9d2aaab0f872249a905a4aee51e1b0
Robert Godlewski <robtechg5@gmail.com>
2023-03-16 17:57:04 -0700

    fixed tag model to not accept blanks

--- myplugins/tag/models.py
+++ myplugins/tag/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-# Create your models here.
+# Tags are mainly used for categorizing and SEO purposes
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
 
     def __str__(self) -> str:
         return self.name
