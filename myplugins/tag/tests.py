21687c6becd4cb96ad54e092d9102446dde01774
Robert Godlewski <robtechg5@gmail.com>
2024-01-09 13:29:56 -0800

    started a testing group for tags

--- myplugins/tag/tests.py
+++ myplugins/tag/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+#from .models import Tag
 
-# Create your tests here.
+# Need to fix this test
+# class TagModelTests(TestCase):
+#     def duplicate_tag_case(self):
+#         name = 'test'
+#         test = Tag.objects.create(name=name)
+#         test.save()
+#         dup = Tag.objects.create(name=name)
+#         dup.save()
+#         self.assertEqual(test.name, dup.name)
