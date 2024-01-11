17e4dbfc3cda686cac7f2f26a67c682eb9837527
Robert Godlewski <robtechg5@gmail.com>
2024-01-11 13:46:48 -0800

    starting to add in views and urls

--- myplugins/myplugins/urls.py
+++ myplugins/myplugins/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
     # path('registration/', include('registration.urls')),
     path('tag/', include('tag.urls')),
     path('crm/', include('crm.urls')),
--- myplugins/registration/views.py
+++ myplugins/registration/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    # For now just have the whole side reference the admin
+    return redirect('/admin/login/')
